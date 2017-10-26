7268c19f5516cf4c5c58e0acbbe33c446dd4966b
Nathalie TRAN <nathalie.tran@telecom-paristech.fr>
2017-10-26 21:10:41 +0200

    send message1

--- routes/webhook.js
+++ routes/webhook.js
@@ -27,10 +27,16 @@ router.post('/', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
       // Get the sender PSID
-      let sender_psid = event.sender.id;
+      let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
 
-      chatService.sendTextMessage(event.sender.id, event.message.text);
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        chatService.sendTextMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Return a '200 OK' response to all events
